7ad8d7b5804c771d668453f970cdef6bf4810ed3
wojlin <wojciech.linowski.2137@gmail.com>
2023-03-06 23:00:39 +0100

    implementing peer to peer com

--- peer_to_peer_com.py
+++ peer_to_peer_com.py
@@ -1,35 +1,37 @@
 from typing import Sequence, Optional
+from dataclasses import dataclass
 from threading import Thread
 
-class PeerToPeerHub:
-    def __init__(self, use_api: bool):
-        self.__use_api = use_api
-        if not use_api:
-            self.__config = self.setup_config()
+from peer_to_peer.Recorder import Recorder, AudioDevice
+@dataclass
+class Config:
+    user_id: str
+    modulation_type: str
 
+
+class PeerToPeerHub:
+    def __init__(self, config:):
         Thread(target=self.__listen).start()
 
     def __listen(self):
         while True:
             pass
 
-    def setup_config(self, config: dict = {}):
-        if self.__use_api:
-            if "user_id" not in config:
-                raise Exception("'user_id' not in config")
 
-            if "modulation_type" not in config:
-                raise Exception("'modulation_type' not in config")
-            return config
+class ConfigCli:
+    def __init__(self):
+        pass
 
+    def setup_config(self):
         user_id = input("user id:")
         modulation_type = input("modulation type:")
 
-        return {"user_id": user_id, "modulation_type": modulation_type}
-
+        return Config(user_id=user_id, modulation_type=modulation_type)
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
-    hub = PeerToPeerHub(use_api=False)
+    config = ConfigCli()
+    
+    hub = PeerToPeerHub(Config)
 
     return 0
 
